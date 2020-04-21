a8438a07fc02c68b646c7c22a77c58b490e368a2
Bala Luguja <bluguja@gmail.com>
2020-04-21 16:30:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -38,7 +38,7 @@ class CashRegister
 
 
   def void_last_transaction()
-    self.total = self.total - @last_transaction_amount
+    @last_transaction_amount = self.total- - -@last_transaction_amount
   end
  end
 
