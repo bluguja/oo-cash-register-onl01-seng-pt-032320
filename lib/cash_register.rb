bee8c9d615aaa3e2199e3c7cc2ab7e55c5db4ee3
Bala Luguja <bluguja@gmail.com>
2020-04-21 15:56:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,9 +20,9 @@ class CashRegister
     else
       @items << title #name
     end
-      def add_item_2(cost, title, number = 1)
+      def add_item_2( title, number = 1)
         
-      self.total = cost * number
+      self.total = @last_transaction_amount * number
     
       end
 
